9c2f05233e39d7237b567f840221eae612ad3b2a
Michael Luca <88971408+SH13LD0@users.noreply.github.com>
2024-03-04 10:32:51 +0000

    fix indentation error

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,4 @@
-class Node:
-  def __init__(self, value):
-    self.value = value
-    self.next = None
-    self.prev = None
-    
-class DoublyLinkedList:
-  def __init__(self, value):
-    new_node = Node(value)
-    self.head = new_node
-    self.tail = new_node
-    self.length = 1
-    
-  def print_list(self):
-    temp = self.head
-    while temp:
-      print(temp.value)
-      temp = temp.next
-        
-  def append(self, value):
+def append(self, value):
     new_node = Node(value)
     if self.head is None:
       self.head = new_node
