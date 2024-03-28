0509c9bf4a81f12d1a4b31680ea79345fb4c0914
Michael Luca <88971408+SH13LD0@users.noreply.github.com>
2024-03-28 21:01:53 +0000

    add get_item method

--- hash_table.py
+++ hash_table.py
@@ -17,7 +17,12 @@ class HashTable:
         if self.data_map[index] == None:
             self.data_map[index] = []
         self.data_map[index].append([key, value])
-            
-my_table = HashTable()
-my_table.set_item("cars", 100)
-my_table.print_table()
+        
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+    
