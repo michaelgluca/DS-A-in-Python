bbe716478972ec2ac6c1e630bb7bc5b28c61f2e1
Michael Luca <88971408+SH13LD0@users.noreply.github.com>
2024-04-01 15:07:46 +0100

    fix keys method

--- hash_table.py
+++ hash_table.py
@@ -30,6 +30,6 @@ class HashTable:
         all_keys = []
         for i in range(len(self.data_map)):
             if self.data_map[i] is not None:
-                for j in range(self.data_map[i]):
+                for j in range(len(self.data_map[i])):
                     all_keys.append(self.data_map[i][j][0])
         return all_keys
