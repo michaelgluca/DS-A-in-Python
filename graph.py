1bcfc638cf125a29cf997768eb56330facd6b003
Michael Luca <michael.luca.msc@gmail.com>
2024-04-23 15:07:03 +0100

    add add_edge method

--- graph.py
+++ graph.py
@@ -11,3 +11,10 @@ class Graph:
     def print_graph(self):
         for vertex in self.adj_list:
             print(vertex, ':', self.adj_list[vertex])
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
