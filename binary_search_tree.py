e2c948ee9f110527f20b1ec85a5ebc0e4b94fa58
michaelgluca <michael.luca.msc@gmail.com>
2024-05-18 15:33:17 +0100

    add dfs_pre_order method

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -54,3 +54,16 @@ class BinarySearchTree:
             if current_node.right is not None:
                 queue.append(current_node.right)
         return result
+
+    def dfs_pre_order(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
