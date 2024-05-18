c0b7769ef01b8b0840f715ab40a9ec0401e5d9ce
michaelgluca <michael.luca.msc@gmail.com>
2024-05-18 14:28:24 +0100

    add BFS method

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -39,3 +39,18 @@ class BinarySearchTree:
             else:
                 return True
         return False
+
+    def bfs(self):
+        current_node = self.root
+        queue = []
+        result = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            result.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return result
--- recursion.py
+++ recursion.py
@@ -63,3 +63,18 @@ def __delete_node(self, current_node, value):
 
 def delete_node(self, value):
     self.root = self.__delete_node(self.root, value)
+
+
+def invert(self):
+    self.root = self.__invert_tree(self.root)
+
+
+def __invert_tree(self, node):
+    if node is None:
+        return None
+
+    temp = node.left
+    node.left = self.__invert_tree(node.right)
+    node.right = self.__invert_tree(temp)
+
+    return node
