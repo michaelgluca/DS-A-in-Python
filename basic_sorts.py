61a3824d91a478918424a8fde340d1135720e5c8
michaelgluca <michael.luca.msc@gmail.com>
2024-05-21 18:33:47 +0100

    add sorts file

---
+++ basic_sorts.py
@@ -0,0 +1,27 @@
+class Sorts:
+    def bubble_sort(self, my_list):
+        for i in range(len(my_list) - 1, 0, -1):
+            for j in range(i):
+                if my_list[j] > my_list[j + 1]:
+                    my_list[j], my_list[j + 1] = my_list[j + 1], my_list[j]
+        return my_list
+
+    def selection_sort(self, my_list):
+        for i in range(len(my_list) - 1):
+            min_index = i
+            for j in range(i + 1, len(my_list)):
+                if my_list[j] < my_list[min_index]:
+                    min_index = j
+            if i != min_index:
+                my_list[i], my_list[min_index] = my_list[min_index], my_list[i]
+        return my_list
+
+    def insertion_sort(self, my_list):
+        for i in range(1, len(my_list)):
+            temp = my_list[i]
+            j = i - 1
+            while temp < my_list[j] and j > - 1:
+                my_list[j + 1] = my_list[j]
+                my_list[j] = temp
+                j -= 1
+        return my_list
